a9c679fe2505bb742265a2dc28fd708b9b8cfa73
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2023-03-23 12:42:54 +0100

    test

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -43,3 +43,51 @@ def test_multi_xmlns_works():
         (isinstance(x, SpeciesSymbol) and x.representation_type == "sum")
         for x in sym.free_symbols
     )
+
+
+def test_abs():
+    ast = sympy_to_sbml_math(sp.Abs(sp.Symbol("a")))
+    assert ast.isFunction()
+    assert ast.getType() == libsbml.AST_FUNCTION_ABS
+
+
+def test_math_dimensionless():
+    xml = SBMLMathMLPrinter(literals_dimensionless=False).doprint(sp.Integer(1))
+    assert "dimensionless" not in xml
+
+    xml = SBMLMathMLPrinter(literals_dimensionless=True).doprint(sp.Integer(1))
+    assert "dimensionless" in xml
+
+    # verify that it's parsable with sbml annotations
+    sbml_math_to_sympy(sympy_to_sbml_math(sp.Integer(1)))
+
+
+@pytest.mark.xfail(strict=True)
+def test_libsbml_mathml_drops_multi_xmlns():
+    mathml = (
+        '<?xml version="1.0" encoding="UTF-8"?>'
+        '<math xmlns="http://www.w3.org/1998/Math/MathML" '
+        'xmlns:multi="http://www.sbml.org/sbml/level3/version1/multi/version1">'
+        '<ci multi:representationType="sum"> a </ci>'
+        "</math>"
+    )
+    assert "xmlns:multi" in mathml
+    ast_node = libsbml.readMathMLFromString(mathml)
+    new_mathml = libsbml.writeMathMLToString(ast_node)
+    print(new_mathml)
+    assert "xmlns:multi" in new_mathml
+
+
+def test_libsbml_mathml_preserves_sbml_xmlns():
+    mathml = (
+        '<?xml version="1.0" encoding="UTF-8"?>'
+        '<math xmlns="http://www.w3.org/1998/Math/MathML" '
+        'xmlns:sbml="http://www.sbml.org/sbml/level3/version2/core">'
+        '<cn sbml:units="dimensionless">2</cn>'
+        "</math>"
+    )
+    assert "xmlns:sbml" in mathml
+    ast_node = libsbml.readMathMLFromString(mathml)
+    new_mathml = libsbml.writeMathMLToString(ast_node)
+    print(new_mathml)
+    assert "xmlns:sbml" in new_mathml
